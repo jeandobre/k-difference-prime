20f0bcb44e1a43457d59ab4a38ee177d4620f5cc
jeandobre <jeandobre@gmail.com>
2016-05-31 11:08:44 -0400

    Corrigido alguns bugs, melhorado o acoplamento entre as classes e criado arquivo auxiliar.h para juntar as classes comuns.

---
+++ auxiliar.h
@@ -0,0 +1,161 @@
+#include <fstream>
+#include <sstream>
+#include <string.h>
+
+//constantes definidas para uso de mensagens na tela
+#define ERR_ARGS "Uso correto:\n -a -alpha -pattern -padrao -p -P \t(String) \n -b -beta -text -texto -t -T \t\t(String) \n -k -K \t\t\t\t\t(int) maior que 0 e menor que m\n [-sm] \tmostrar a matriz\n [-vs%] \tversao do algoritmo (1,2 ou 3)\n [-st] \tmostrar o tempo de execucao"
+#define ERR_KMAIOR "O parametro k deve estar em um intervalo entre 0 e m. "
+#define MSG_0_OCCR "\nNao foi encontrado nenhuma ocorrencia com pelo menos "
+#define MSG_N_OCCR "\nOcr "
+
+using namespace std;
+
+class Primer{
+
+private:
+   int ocr;
+   int j;
+   int r;
+   string sequencia;
+
+public:
+   Primer(int ocr, int j, int r, string sequencia){
+      this->ocr = ocr;
+      this->j = ++j;
+      this->r = r;
+      this->sequencia = sequencia;
+   };
+
+   void escreverTela(){
+      cout<< setfill('0');
+      cout<<MSG_N_OCCR<<setw(3)<<ocr<<" em ";
+
+      cout<< setfill(' ');
+      cout<<setw(3)<<j<<".."<<setw(3)<<j + r;
+      cout<<"-Tamanho("<<setw(3)<<sequencia.size()<<"): ";
+      cout<<sequencia<<endl;
+   };
+};
+
+class Parametro{
+
+public:
+   string alpha, beta;
+   int k;
+   bool mostrarMatriz;
+   bool mostrarTempo;
+   int versao;
+
+   int total;
+
+   Parametro(){
+     k = 0;
+     mostrarMatriz = false;
+     mostrarTempo = false;
+     versao = 1;
+     total = 0;
+   };
+
+};
+
+//função auxiliar compara três inteiros e devolve o maior
+unsigned long long int menorDeTres(unsigned long long int x,
+                                   unsigned long long int b,
+                                   unsigned long long int c){
+  if((x <= b) && (x <= c)) return x;
+  else if((b <= x) && (b <= c)) return b;
+  else return c;
+}
+
+//função para abrir arquivo FASTA
+//TODO devemos abrir o arquivo em tempo de execução, ou seja, processar enquanto abre o arquivo,
+ string lerArquivo(const char *local){
+
+  ifstream arquivo(local, std::ifstream::in);
+
+  if(!arquivo.good()) {
+    arquivo.close();
+
+    return local;
+  }
+  cout<<"Abrindo arquivo "<<local<<"...\n";
+
+  std::ostringstream contents;
+  contents << arquivo.rdbuf();
+
+  arquivo.close();
+
+  return(contents.str());
+}
+
+//função auxiliar recebe os parâmetros que o usuário digitou, valida e transforma os valores
+Parametro *parseParametros(int argc, char** argv){
+   string argA[6] = {"-a", "-alpha", "-pattern", "-padrao", "-p", "-P"};
+   string argB[6] = {"-b", "-beta", "-text", "-texto", "-t", "-T"};
+   string argK[2] = {"-k", "-K"};
+
+   Parametro *p = new Parametro();
+
+   int temA, temB, temK;
+   temA = temB = temK = 0;
+
+   for(int z = 1; z < argc; z++){
+      if(strcmp(argv[z], "-sm") == 0){
+        //if(argc == 7) return 0;
+        p->mostrarMatriz = true;
+        continue;
+      }
+
+      //versão vs1 = Matriz Completa, vs2 = Melhorada 1 e vs3 Molhorada 2
+      if(strcmp(argv[z], "-vs1") == 0){
+        //if(argc == 7) return 0;
+        p->versao=1;
+        continue;
+      }
+      if(strcmp(argv[z], "-vs2") == 0){
+        //if(argc == 7) return 0;
+        p->versao=2;
+        continue;
+      }
+
+      if(strcmp(argv[z], "-vs3") == 0){
+        //if(argc == 7) return 0;
+        p->versao=3;
+        continue;
+      }
+
+      if(strcmp(argv[z], "-st") == 0){
+        //if(argc == 7) return 0;
+        p->mostrarTempo=true;
+        continue;
+      }
+
+      for(int w = 0; w < 6; w++){
+        if(!temA && argA[w].compare(argv[z]) == 0){
+            p->alpha = argv[z + 1];
+            temA = 1;
+            continue;
+        }
+        else if(!temB && argB[w].compare(argv[z]) == 0){
+            p->beta = argv[z + 1];
+            temB = 1;
+            continue;
+        }
+        else if(w < 2 && !temK && argK[w].compare(argv[z]) == 0){
+            try{
+              p->k = atoi(argv[z + 1]);
+              if(p->k > 0) temK = 1;
+              continue;
+            }catch(int e){
+             // return 0;
+            }
+        }
+      }
+   }
+   p->total = temA + temB + temK;
+
+   p->alpha = lerArquivo(p->alpha.c_str());
+   p->beta  = lerArquivo(p->beta.c_str());
+
+   return p;
+}
--- kdifferenceprime1.cpp
+++ kdifferenceprime1.cpp
@@ -16,22 +16,22 @@
  * A versão otimizada traz a i´deia de implementar a matriz D com    *
  * apenas 2 linhas [2 x n] que é o mínimo necessário para que a      *
  * recorrência aconteça. Essa versão diminui muito o uso de memória  *
- * mas não de processamento.                                         *
+ * mas não de processamento.
+ *
+ * Atualizado em 09/03/2016 versão com opt-2 que deve computar a     *
+ * matriz  D1..m + 1 pivo e impentantação de leitura arquivo FASTA   *
+ * e agrupamento em forma de projeto, removendo as funções auxilares *
+ * para uma biblioteca de uso geral e por último a implementação de  *
+ * um vetor<string> para guardar os resultados indexando pelo sufixo *
  * *******************************************************************/
+
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
 #include <iomanip>
 #include <time.h>
-#include <fstream>
-
-using namespace std;
-
-//constantes definidas para uso de mensagens na tela
-#define ERR_ARGS "Uso correto:\n -a -alpha -pattern -padrao -p -P \t(String) \n -b -beta -text -texto -t -T \t\t(String) \n -k -K \t\t\t\t\t(int) maior que 0 e menor que m\n [-sm] \tmostrar a matriz\n [-opt] \tversão otimizada\n [-st] \tmostrar o tempo de execucao"
-#define ERR_KMAIOR "O parametro k deve estar em um intervalo entre 0 e m. "
-#define MSG_0_OCCR "\nNao foi encontrado nenhuma ocorrencia com pelo menos "
-#define MSG_N_OCCR "\nOcorrencia "
+#include <list>
+#include "auxiliar.h"
 
 //declaração da classe que é utilizada no KdifferencePrime
 class KdifferenceInexactMatch1;
@@ -43,95 +43,20 @@ class KdifferencePrime{
       int k;             //quantidade de diferênças
       int m;             //tamanho de alpha
       int n;             //tamanho de beta
-      int mostrarMatriz; //booleano 0,1 usado para imprime a matriz na tela
-      int otimizado;     //booleano 0, 1 usado para executar a versão otimizada do algoritmo
-      bool tempo;        //booleano 0, 1 usado para imprime o tempo na tela (O tempo é calculado independentemente)
+      bool mostrarMatriz; //booleano 0,1 usado para imprime a matriz na tela
+      int versao;        //booleano 0, 1 usado para executar a versão otimizada do algoritmo
+
       KdifferenceInexactMatch1 *c;
+      list<Primer *> primers;
 
     public:
       KdifferencePrime(){
-         //construtor seta os valores default se o usuário não escolher nada
-         this->mostrarMatriz=0;
-         this->otimizado=0;
-         this->tempo=false;
+          //construtor seta os valores default se o usuário não escolher nada
+         this->mostrarMatriz=false;
+         this->versao=3; //começa com a melhor versão
       }
       void processar();
 } prime; // é necessário apenas uma instancia de prime, já declarada aqui
-
-//função auxiliar compara três inteiros e devolve o maior
-int maior(int x, int b, int c){
-  if((x <= b) && (x <= c)) return x;
-  else if((b <= x) && (b <= c)) return b;
-  else return c;
-}
-
-//função auxiliar recebe os parâmetros que o usuário digitou, valida e transforma os valores
-int parseParametros(int argc, char** argv){
-   string argA[6] = {"-a", "-alpha", "-pattern", "-padrao", "-p", "-P"};
-   string argB[6] = {"-b", "-beta", "-text", "-texto", "-t", "-T"};
-   string argK[2] = {"-k", "-K"};
-
-   int temA, temB, temK;
-   temA = temB = temK = 0;
-
-   for(int z = 1; z < argc; z++){
-      if(strcmp(argv[z], "-sm") == 0){
-        if(argc == 7) return 0;
-        prime.mostrarMatriz=1;
-        continue;
-      }
-
-      if(strcmp(argv[z], "-opt") == 0){
-        if(argc == 7) return 0;
-        prime.otimizado=1;
-        continue;
-      }
-
-      if(strcmp(argv[z], "-st") == 0){
-        if(argc == 7) return 0;
-        prime.tempo=true;
-        continue;
-      }
-
-      for(int w = 0; w < 6; w++){
-        if(!temA && argA[w].compare(argv[z]) == 0){
-            prime.alpha = argv[z + 1];
-            temA = 1;
-            continue;
-        }
-        else if(!temB && argB[w].compare(argv[z]) == 0){
-            prime.beta = argv[z + 1];
-            temB = 1;
-            continue;
-        }
-        else if(w < 2 && !temK && argK[w].compare(argv[z]) == 0){
-            try{
-              prime.k = atoi(argv[z + 1]);
-              if(prime.k > 0) temK = 1;
-              continue;
-            }catch(int e){
-              return 0;
-            }
-        }
-      }
-   }
-   int cc = temA + temB + temK;
-
-   ifstream arquivo(prime.alpha.c_str());
-
-   if (arquivo.is_open()){
-     getline(arquivo, prime.alpha);
-   }
-   arquivo.close();
-
-   if (arquivo.is_open()){
-     getline(arquivo, prime.beta);
-   }
-   arquivo.close();
-
-   return cc;
-}
-
 
 //O algoritmo foi adaptado para entregar o resultado do primer que é o inverso da programação original
 //Classe que resolve o problema k-mismatches problems em tempo O(mn)
@@ -149,7 +74,7 @@ class KdifferenceInexactMatch1{
     int linha;    //linha que contém todos os valores >= k
 
   public:
-    KdifferenceInexactMatch1(string a, string t, int k){
+    KdifferenceInexactMatch1(string &a, string &t, int &k){
        this->a = a;
        this->t = t;
        this->k = k;
@@ -187,7 +112,7 @@ class KdifferenceInexactMatch1Original: public KdifferenceInexactMatch1{
     /**
       este método tem a responsabilidade de desalocar a última linha da matriz D
       quando uma nova string 'a' for alocada. Isso garante que a primeira chamada do algoritmo
-      usa a maior quantidade de memória possível (m+1 x n+1) e a cada nova chamada
+      usa a maior quantidade de memória já alocada (m+1 x n+1) e a cada nova chamada
       uma linha 'desnecessária' é removida
     */
     void setA(string a){
@@ -202,10 +127,10 @@ class KdifferenceInexactMatch1Original: public KdifferenceInexactMatch1{
     }
 };
 
-//implementação original Matriz D[0..1, 0..n]
-class KdifferenceInexactMatch1Optimizado: public KdifferenceInexactMatch1{
+//implementação Matriz D[0..1, 0..n]
+class KdifferenceInexactMatch1Optimizado1: public KdifferenceInexactMatch1{
   public:
-   KdifferenceInexactMatch1Optimizado(string a, string t, int k):KdifferenceInexactMatch1(a,t,k){
+   KdifferenceInexactMatch1Optimizado1(string a, string t, int k):KdifferenceInexactMatch1(a,t,k){
 
        //alocação de 2 linhas de inteiros por n + 1
        this->D = new (nothrow) int*[2];
@@ -215,9 +140,35 @@ class KdifferenceInexactMatch1Optimizado: public KdifferenceInexactMatch1{
        this->rowPrint = 1;
    }
 
-   ~KdifferenceInexactMatch1Optimizado(){
+   ~KdifferenceInexactMatch1Optimizado1(){
        delete [] D[0];
        delete [] D[1];
+       delete [] D;
+    }
+
+   void executar();
+
+   //aqui não há a necessidade de desalocar a última linha, pois a matriz já é mínima
+   void setA(string a){
+      this->a = a;
+      this->m = a.size();
+    }
+};
+
+//implementação Matriz D[0, 0..n]
+class KdifferenceInexactMatch1Optimizado2: public KdifferenceInexactMatch1{
+  public:
+   KdifferenceInexactMatch1Optimizado2(string a, string t, int k):KdifferenceInexactMatch1(a,t,k){
+
+       //alocação de 1 linha de inteiros por n + 1
+       this->D = new (nothrow) int*[1];
+       this->D[0] = new (nothrow) int[n + 1];
+
+       this->rowPrint = 0;
+   }
+
+   ~KdifferenceInexactMatch1Optimizado2(){
+       delete [] D[0];
        delete [] D;
     }
 
@@ -265,23 +216,23 @@ void KdifferenceInexactMatch1Original::executar(){
 
    bool passou; //flag para verificar se toda a linha tem valores >= k
    linha = -1;  //flag que marca a primeira linha em que a condição acima foi satisfeita
-	for(i = 1; i <= m; i++){ /** for i <- 1 to m */
+	for(i = 1; i <= m && linha == -1; i++){ /** for i <- 1 to m */
         passou = true; //começa com aceitando que a linha toda é valida
 
 		for(l = 1; l <= n; l++){ /** for l <- 1 to n */
             /** relação de recorrência */
-			D[i][l] = maior(D[i-1][l] + 1,
+			D[i][l] = menorDeTres(D[i-1][l] + 1,
                             D[i][l-1] + 1,
                             a.compare(i-1, 1, t, l-1, 1) == 0 ? D[i-1][l-1] : D[i-1][l-1] + 1);
 
-			if(D[i][l] < k) passou = false; //se algum valor da linha é < k essa linha é descartada
+			if(passou && D[i][l] < k) passou = false; //se algum valor da linha é < k essa linha é descartada
 		}
         if(passou && linha == -1) linha = i; //se a linha não foi descartada e ela é a primeira, guarda ela
 	}
 }
 
 //método que executa o comportamento otimizado do algoritmo
-void KdifferenceInexactMatch1Optimizado::executar(){
+void KdifferenceInexactMatch1Optimizado1::executar(){
    int l;
 
    for(l = 0; l <= n; l++) /** necessário inicializar apenas a primeira linha */
@@ -291,22 +242,46 @@ void KdifferenceInexactMatch1Optimizado::executar(){
    linha = -1;  //flag que marca a primeira linha em que a condição acima foi satisfeita
    for(int i = 1; i <= m && linha == -1; i++){
      D[1][0] = i;
-	 for(l = 1; l <= n; l++)
-        D[1][l] = maior(D[0][l] + 1,
-                        D[1][l-1] + 1,
-                        a.compare(i-1, 1, t, l-1, 1) == 0 ? D[0][l-1] : D[0][l-1] + 1);
-
-     /**
-       aqui vai verificar se a linha tem todos os valores >= k
-       ocorre também o transporte dos valores da linha 1 para 0 a cada nova iteração
-       não há necessidade de terminar a execução se a linha já foi alcançada por isso o for tem a condição 'linha == -1'
-     */
      passou = true;
-	 for(l = 0; l <= n; l++){
+	 for(l = 1; l <= n; l++){
+        D[1][l] = menorDeTres(D[0][l]   + 1,
+                              D[1][l-1] + 1,
+                              a.compare(i-1, 1, t, l-1, 1) == 0 ? D[0][l-1] : D[0][l-1] + 1);
+
         if(passou && D[1][l] < k) passou = false; //se algum valor da linha é < k essa linha é descartada
-        D[0][l] = D[1][l];
-        D[1][l] = 0;
+        //aqui reorganiza a matriz para o próximo laço
+        D[0][l-1] = D[1][l-1];
+        D[1][l-1] = 0;
      }
+     D[0][n] = D[1][n];
+     D[1][n] = 0;
+     if(passou) linha = i; //se a linha não foi descartada ela é a primeira
+   }
+}
+
+//método que executa o comportamento otimizado do algoritmo
+void KdifferenceInexactMatch1Optimizado2::executar(){
+   int l;
+
+   for(l = 0; l <= n; l++) /** necessário inicializar apenas a primeira linha */
+	  D[0][l] = 0;
+
+   bool passou; //flag para verificar se toda a linha tem valores >= k
+   linha = -1;  //flag que marca a primeira linha em que a condição acima foi satisfeita
+   unsigned long long int pivo;
+   unsigned long long int aux;
+
+   for(int i = 1; i <= m && linha == -1; i++){
+     pivo = i;
+     passou = true;
+	 for(l = 1; l <= n; l++){
+        aux = menorDeTres(D[0][l] + 1,
+                             pivo + 1,
+                             a.compare(i-1, 1, t, l-1, 1) == 0 ? D[0][l-1] : D[0][l-1] + 1);
+	    D[0][l-1] = pivo;
+	    if(passou && pivo < k) passou = false;
+	    pivo = aux;
+	 }
      if(passou) linha = i; //se a linha não foi descartada ela é a primeira
    }
 }
@@ -316,9 +291,11 @@ void KdifferenceInexactMatch1Optimizado::executar(){
 void KdifferencePrime::processar(){
    int ocr = 0; //flag que guarda a quantidade de ocorrências
 
-   if(otimizado) //se o usuário escolheu a versão otimizada
-      c = new KdifferenceInexactMatch1Optimizado(alpha, beta, k); //instancia a classe Otimizada
-   else
+   if(versao == 3) //se o usuário escolheu a versão otimizada 2
+      c = new KdifferenceInexactMatch1Optimizado2(alpha, beta, k); //instancia a classe Otimizada
+   else if(versao == 2) //se o usuário escolheu a versão otimizada
+      c = new KdifferenceInexactMatch1Optimizado1(alpha, beta, k); //instancia a classe Otimizada
+   else  //se o usuário escolheu a versão normal
       c = new KdifferenceInexactMatch1Original(alpha, beta, k);  //instancia a classe Original
 
    /*
@@ -326,15 +303,17 @@ void KdifferencePrime::processar(){
      pois é necessário k caracteres para existir comparação
    */
    for(int j = 0; j < (m - k) + 1; j++){
+       cout<<j<<",";
        c->setA(alpha.substr(j));
        c->executar();
 
        if(c->linha > -1){ //se a linha foi obtida, significa que existe uma ocorrência
-           cout<<MSG_N_OCCR<<++ocr<<" em: "<<j<<", "<<j + c->linha<<" "<<alpha.substr(j, c->linha)<<endl;
-           if(mostrarMatriz) c->imprimirMatrizTela();
+           Primer *pr = new Primer(++ocr,j,c->linha,alpha.substr(j, c->linha));
+          // pr->escreverTela();
+           primers.insert(primers.end(), pr);
+          // if(mostrarMatriz) c->imprimirMatrizTela();
        }
    }
-   if(ocr == 0) cout<<MSG_0_OCCR<<k<<" diferenca(s)"<<endl;
 }
 
 int main(int argc, char** argv) {
@@ -344,10 +323,16 @@ int main(int argc, char** argv) {
 	  return 0;
    }
 
-   if(parseParametros(argc, argv) != 3){
+   Parametro *p = parseParametros(argc, argv);
+   if(p->total != 3){
       cout<<endl<<ERR_ARGS<<endl;
 	  return 0;
    }
+   prime.alpha = p->alpha;
+   prime.beta = p->beta;
+   prime.k = p->k;
+   prime.mostrarMatriz = p->mostrarMatriz;
+   prime.versao = p->versao;
 
    prime.m = prime.alpha.size();
    prime.n = prime.beta.size();
@@ -357,13 +342,19 @@ int main(int argc, char** argv) {
      return 0;
    }
 
+   cout<<"K-difference-primer-1 processando..."<<endl;
+   cout<<"Versao do algoritmo: "<<prime.versao<<endl;
+
    time_t inicio, fim;
 
    inicio = clock();
    prime.processar();
    fim = clock();
 
-   if(prime.tempo)
+   if(prime.primers.size() == 0) cout<<MSG_0_OCCR<<prime.k<<" diferenca(s)"<<endl;
+   else cout<<"Encontrado "<<prime.primers.size()<<" ocorrencia(s) r de primers"<<endl;
+
+   if(p->mostrarTempo)
      cout<<"Tempo de execucao: "<< ((fim - inicio) / (CLOCKS_PER_SEC / 1000)) << " milisegundos";
 
    return 1;
