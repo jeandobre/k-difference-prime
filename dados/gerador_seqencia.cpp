021f6e3d026824b24c138742150b36d9b158eb56
Jean Alexandre Dobre <jeandobre@gmail.com>
2016-09-09 18:23:57 -0400

    Adicionado novos arquivos de dados para testes, 5.000, 10.000 e 20.000.

--- dados/gerador_seqencia.cpp
+++ dados/gerador_seqencia.cpp
@@ -35,11 +35,11 @@ void gerar(string arquivo, int tamanho, const char *a){
 }
 
 int main(){
-    int sizet = 100000;
-    gerar("alfa_100000.txt", sizet);
-    gerar("beta_100000.txt", sizet);
-    gerar("alfa_A100000.txt", sizet, "A");
-    gerar("beta_C100000.txt", sizet, "C");
+    int sizet = 20000;
+    gerar("alfa_20000.txt", sizet);
+    gerar("beta_20000.txt", sizet);
+    gerar("alfa_A20000.txt", sizet, "A");
+    gerar("beta_C20000.txt", sizet, "C");
     return 0;
 
 }
