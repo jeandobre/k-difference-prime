8a05be21eb6567c6fa0e6c53556e16b88d480759
Jean Alexandre Dobre <jeandobre@gmail.com>
2016-09-11 17:13:34 -0400

    Atualizado versão modificada do algoritmo 1 na tentativa de diminuir a quantidade de execuções

--- kdifferenceprime1md.cpp
+++ kdifferenceprime1md.cpp
@@ -32,11 +32,6 @@
 #include <list>
 #include "classes.h"
 
-struct JRprime{
-   int j;
-   int r;
-};
-
 //implementação Matriz D[0, 0..n]
 class KdifferenceInexactMatch1Modificado: public KdifferenceInexactMatch{
   protected:
@@ -59,13 +54,12 @@ class KdifferenceInexactMatch1Modificado: public KdifferenceInexactMatch{
         return "K1md";
     }
 
-   int executar(int m);
+   int executar(int m, int j);
 };
 
 class KdifferencePrime1: public KdifferencePrime{
 
     public:
-      JRprime jr;
       void instanciar(){
           c = new KdifferenceInexactMatch1Modificado(alpha, beta, &k);  //instancia a classe Original
       }
@@ -74,29 +68,17 @@ class KdifferencePrime1: public KdifferencePrime{
         instanciar(); //essa chamada depende diretamente da implementação do kdifferenceInexactMatch que será utilizado
          //O(m), onde m é o tamanho de alpha
          j = 0;
-         //while(j < (m - k) + 1){
-         while(j < 5){
+         while(j < (m - k) + 1){
              //guardo em um array todos as ocorrências r de primer para cada j
-             jr.r = c->executar(m - j);
-             if(jr.r > 0){ // então há uma ocorrencia de primer
-                if(jr.j > 1){//então há mais ocorrências de primer para o mesmo r
-                  for(int v = 0;v <= (jr.j - 2); v++, j++)
-                    ocr[j] = jr.r--;
-                } else {
-                  ocr[j] = jr.r;
-                  j++;
-                }
-             } else{
-              ocr[j] = jr.r;
-              j++;
-             }
+             cout<<j<<" ";
+             c->executar(m - j, j);
+             j += jr[j].Jmax;
          }
-
       }
 } prime; // é necessário apenas uma instancia de prime, já declarada aqui
 
 //método que executa o comportamento otimizado do algoritmo
-int KdifferenceInexactMatch1Modificado::executar(int m){
+int KdifferenceInexactMatch1Modificado::executar(int m, int j){
    this->m = m;
    this->rowPrint = m;
    int l;
@@ -107,19 +89,13 @@ int KdifferenceInexactMatch1Modificado::executar(int m){
    bool passou;     //flag para verificar se toda a linha tem valores >= k
    int Imax = -1;  //flag que marca última linha que tem algum D[i,j] = k-1
    int Jmax = -1;  //maior linha com 0 diferenças = Maior alcance do LCE(0,0..n)
-   int pivo,aux;
+   int pivo, aux, diagonal1, diagonal2;
    char tt;
-
- //  fstream out;
- //  out.open("matriz.csv", ios::out | ios::trunc);
- //  out<<",";
-
- //  for(int i = 0; i <= m; i++) out<<t[i]<<",";
-//	out<<endl;
 
    for(int i = 1; i <= m && Imax == -1; i++){
      pivo = i; passou = true;
      tt = a[prime.j + i-1];
+     diagonal1 = -(k+1);
 	  for(l = 1; l <= n; l++){
        aux = menorDeTres(D[l] + 1,
                          pivo + 1,
@@ -127,21 +103,26 @@ int KdifferenceInexactMatch1Modificado::executar(int m){
 	    D[l-1] = pivo;
 	    pivo = aux;
 	    if(passou && pivo < k) passou = false;
-	    if(pivo == 0) Jmax = i;
+
+	    if(pivo == 0){
+	       Jmax = i;
+          diagonal1 = l-i;
+       }
 	  }
 	  D[n] = aux;
-     if(passou) Imax = i; //se a linha não foi descartada ela é a primeira
-
-  //   out<<a[prime.j + i - 1]<<",";
- //    for(int l = 0; l <= n; l++){
-//			out<<D[l]<<","; //imprimi o valor da matriz
-//		}
-	//	out<<endl;
+     if(passou){
+       Imax = i; //se a linha não foi descartada ela é a primeira
+       //busca o maior j
+       if(Jmax > 1 && Imax + diagonal1 <= n && D[Imax + diagonal1] == k){
+         Jmax = 1;
+       }
+     }
    }
-   prime.jr.j = Jmax-1;
-   prime.jr.r = Imax;
- //  out.close();
-   //return Imax == -1 ? Imax : --Imax; //por conta do índice 1 da sequencia começar em zero
+
+   prime.jr[j].j    = prime.j;
+   prime.jr[j].Jmax = Jmax <= 0 ? 1: Jmax;
+   prime.jr[j].r    = Imax;
+
    return Imax;
 }
 
@@ -177,18 +158,21 @@ int main(int argc, char** argv) {
    time_t inicio, fim;
 
    time(&inicio);
-   if(prime.tempo) formataTempo(inicio, true);
+   if(prime.tempo) formataTempo(inicio, "Inicio");
    prime.processar();
 
    time(&fim);
-   if(prime.tempo) formataTempo(fim, false);
+   if(prime.tempo) formataTempo(fim, "Fim   ");
 
-   if(!prime.mostrarMatriz) prime.mostrarOcorrencias();
+   prime.gerarOcorrencias();
+   prime.mostrarOcorrencias();
 
    if(prime.tempo){
      double seconds = difftime(fim, inicio);
      formataSegundos(seconds);
+     mostrarMemoria();
    }
+
 
    return 1;
 }
